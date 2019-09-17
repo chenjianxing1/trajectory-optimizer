a4ca0d73ca49b1eafb8224de9065ec01f31eb4d8
Patrick <patrickhart.1990@gmail.com>
2019-09-18 00:40:50 +0200

    snapshot

--- src/optimizer.h
+++ src/optimizer.h
@@ -5,6 +5,7 @@
 // For a copy, see <https://opensource.org/licenses/MIT>.
 #pragma once
 #include <vector>
+#include <ceres/ceres.h>
 #include <algorithm>
 #include "src/commons/parameters.h"
 #include "src/geometry/geometry.h"
@@ -16,22 +17,25 @@ using std::vector;
 
 class Optimizer {
  public:
-  explicit Optimizer(Parameters& params) : params_(params) {}
+  explicit Optimizer(Parameters& params) :
+    params_(params),
+    optimization_vectors_(),
+    parameter_block_() {}
 
   // TODO(@hart): every functor should have access to the params!
   template<typename T>
   void AddResidualBlock(
     const ceres::DynamicAutoDiffCostFunction<T, 4>& functor,
     const ceres::LossFunction* loss = new ceres::TrivialLoss()) {
-    for (auto vec : optimization_vectors_) {
-      functor.AddParameterBlock(vec.length());
+    for (vector<double>& vec : optimization_vectors_) {
+      functor.AddParameterBlock(vec.size());
     }
     problem_.AddResidualBlock(functor, loss, parameter_block_);
   }
 
   // each column of an Eigen Matrix will become an optimization vector
   void AddInputs(Matrix_t<double> inputs) {
-    for (vector<double>& opt_vec :  optimization_vectors_) {
+    for (vector<double>& opt_vec : optimization_vectors_) {
       vector<double> empty_vec(inputs.rows(), 0.0);
       opt_vec.push_back(empty_vec);
       parameter_block_.push_back(&opt_vec[0]);
